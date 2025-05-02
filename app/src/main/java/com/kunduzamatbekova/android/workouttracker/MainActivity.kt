3bd4db0d9f7c606c58ea0ed5a27e42b0d4d74680
kunduz14 <49558346+kunduz14@users.noreply.github.com>
2025-05-02 14:57:44 +0400

    Create Auth Screen

--- app/src/main/java/com/kunduzamatbekova/android/workouttracker/MainActivity.kt
+++ app/src/main/java/com/kunduzamatbekova/android/workouttracker/MainActivity.kt
@@ -4,13 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.kunduzamatbekova.android.workouttracker.auth.AuthScreen
 import com.kunduzamatbekova.android.workouttracker.ui.theme.WorkoutTrackerTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +13,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             WorkoutTrackerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AuthScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WorkoutTrackerTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/kunduzamatbekova/android/workouttracker/auth/AuthScreen.kt
@@ -0,0 +1,168 @@
+package com.kunduzamatbekova.android.workouttracker.auth
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun AuthScreen() {
+    var isLoginScreen by remember { mutableStateOf(true) }
+    val primaryColor = Color(0xFF00658A)
+    val secondaryColor = Color.White
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 160.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        AuthButtonRow(
+            isLoginScreen,
+            primaryColor,
+            secondaryColor,
+            onLoginClick = { isLoginScreen = true },
+            onRegisterClick = { isLoginScreen = false }
+        )
+        Spacer(modifier = Modifier.height(24.dp))
+
+        if (isLoginScreen) {
+            LoginScreen()
+        } else {
+            RegisterScreen()
+        }
+    }
+}
+
+@Composable
+fun LoginScreen() {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier.padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            placeholder = { Text("Email", color = Color.Gray) }
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            placeholder = { Text("Пароль", color = Color.Gray) }
+        )
+        Spacer(modifier = Modifier.height(24.dp))
+        Button(onClick = { }) {
+            Text("Войти")
+        }
+    }
+}
+
+@Composable
+fun RegisterScreen() {
+    var email by remember { mutableStateOf("") }
+    var userName by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier.padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            placeholder = { Text("Email", color = Color.Gray) }
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(
+            value = userName,
+            onValueChange = { userName = it },
+            placeholder = { Text("Имя", color = Color.Gray) }
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            placeholder = { Text("Пароль", color = Color.Gray) }
+        )
+        Spacer(modifier = Modifier.height(24.dp))
+        Button(onClick = { }) {
+            Text("Войти")
+        }
+    }
+}
+
+@Composable
+fun AuthButtonRow(
+    isLoginScreen: Boolean,
+    primaryColor: Color,
+    secondaryColor: Color,
+    onLoginClick: () -> Unit,
+    onRegisterClick: () -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        AuthButton(
+            "Войти",
+            isLoginScreen,
+            primaryColor,
+            secondaryColor,
+            RoundedCornerShape(5.dp, 0.dp, 0.dp, 5.dp),
+            onLoginClick,
+            Modifier.weight(1f)
+        )
+        AuthButton(
+            "Зарегистрироваться",
+            !isLoginScreen,
+            primaryColor,
+            secondaryColor,
+            RoundedCornerShape(0.dp, 5.dp, 5.dp, 0.dp),
+            onRegisterClick,
+            Modifier.weight(1f)
+        )
+    }
+}
+
+@Composable
+fun AuthButton(
+    text: String,
+    isSelected: Boolean,
+    primaryColor: Color,
+    secondaryColor: Color,
+    shape: RoundedCornerShape,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val containerColor = if (isSelected) primaryColor else secondaryColor
+    val contentColor = if (isSelected) secondaryColor else primaryColor
+
+    Button(
+        onClick = onClick,
+        modifier = modifier,
+        shape = shape,
+        colors = ButtonDefaults.buttonColors(containerColor, contentColor),
+        border = BorderStroke(1.dp, primaryColor)
+    ) {
+        Text(text)
+    }
+}
